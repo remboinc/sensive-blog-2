ab083d74f83a147410406def4d96d8e2fb426559
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2024-04-14 19:30:16 +0300

    eplace sorted func with order_by

--- blog/views.py
+++ blog/views.py
@@ -39,9 +39,8 @@ def index(request):
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
-    tags = Tag.objects.all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    popular_tags = Tag.objects.annotate(post_count=Count('posts')).order_by('-post_count')
+    most_popular_tags = popular_tags[:5]
 
     context = {
         'most_popular_posts': [
@@ -80,9 +79,8 @@ def post_detail(request, slug):
         'tags': [serialize_tag(tag) for tag in related_tags],
     }
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    popular_tags = Tag.objects.annotate(post_count=Count('posts')).order_by('-post_count')
+    most_popular_tags = popular_tags[:5]
 
     most_popular_posts = []  # TODO. Как это посчитать?
 
@@ -99,9 +97,8 @@ def post_detail(request, slug):
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    popular_tags = Tag.objects.annotate(post_count=Count('posts')).order_by('-post_count')
+    most_popular_tags = popular_tags[:5]
 
     most_popular_posts = []  # TODO. Как это посчитать?
 

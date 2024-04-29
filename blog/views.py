8f274b90edb424e137ec63092432aedc21d1e4ed
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2024-04-29 12:49:35 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -42,17 +42,17 @@ def serialize_post(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': Post.objects.filter(tags=tag).count(),
+        'posts_with_tag': Post.objects.filter(tags=tag).count()
     }
 
 
 def index(request):
     most_popular_posts = Post.objects.popular() \
-            .prefetch_related('author')[:5] \
+            .prefetch_related('author', 'tags')[:5] \
             .fetch_with_comments_count()
 
     most_fresh_posts = Post.objects.fresh(
-    ).prefetch_related('tags')[:5] \
+    ).prefetch_related('author', 'tags')[:5] \
     .fetch_with_comments_count()
 
     most_popular_tags = Tag.objects.popular()[:5]
@@ -97,7 +97,7 @@ def post_detail(request, slug):
     most_popular_tags = Tag.objects.popular()[:5]
 
     most_popular_posts = Post.objects.popular() \
-            .prefetch_related('author')[:5] \
+            .prefetch_related('tags', 'author')[:5] \
             .fetch_with_comments_count()
 
     context = {
@@ -116,7 +116,7 @@ def tag_filter(request, tag_title):
     most_popular_tags = Tag.objects.popular()[:5]
 
     most_popular_posts = Post.objects.popular() \
-            .prefetch_related('author')[:5] \
+            .prefetch_related('tags', 'author')[:5] \
             .fetch_with_comments_count()
 
     related_posts = tag.posts.annotate(
